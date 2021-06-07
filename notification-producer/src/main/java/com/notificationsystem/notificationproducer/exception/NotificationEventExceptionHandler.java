96ed58652cd022b19ff53b81eb500860e0c32345
Mitesh <mitesh.sit@gmail.com>
2021-06-07 10:09:07 +0530

    added logs in exception

--- notification-producer/src/main/java/com/notificationsystem/notificationproducer/exception/NotificationEventExceptionHandler.java
+++ notification-producer/src/main/java/com/notificationsystem/notificationproducer/exception/NotificationEventExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.notificationsystem.notificationproducer.exception;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,13 +9,15 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@Slf4j
 @RestControllerAdvice
 public class NotificationEventExceptionHandler extends ResponseEntityExceptionHandler {
 
     @ExceptionHandler(NotificationEventException.class)
-    public final ResponseEntity<NotificationApiErrorResponse> handleEmailErrorResponse(
+    public final ResponseEntity<NotificationApiErrorResponse> handleNotificationApiErrorResponse(
             final NotificationEventException ex, final WebRequest webRequest) {
 
+        log.error("Error validating client, status : {}, clientId : {}", ex.getStatus(), ex.getClientId());
         NotificationApiErrorResponse errorResponse =
                 new NotificationApiErrorResponse("500", "Client not registered");
         return new ResponseEntity<>(errorResponse, HttpStatus.INTERNAL_SERVER_ERROR);
